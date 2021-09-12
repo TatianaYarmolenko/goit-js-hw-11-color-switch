46d5124aea65d2b383825c28c384957d62a62e22
ta-moli <yarnusha7@gmail.com>
2021-09-12 16:45:08 +0300

    task1

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+import refs from "./js/refs.js";
+ 
+const colors = [
+   '#FFFFFF',
+   '#2196F3',
+   '#4CAF50',
+   '#FF9800',
+   '#009688',
+   '#795548',
+ ];
 
+refs.startBtn.addEventListener('click', startBtnClick);
+refs.stopBtn.addEventListener('click', stopBtnClick);
+
+let timerId = null;
+
+function startBtnClick() {
+   refs.startBtn.disabled = true;
+ 
+    timerId = setInterval(() => {
+     document.body.style.background = colors[randomIntegerFromInterval(0, colors.length - 1)];
+   }, 1000);
+ };
+ 
+ function stopBtnClick() {
+   clearInterval(timerId);
+   refs.startBtn.disabled = false;
+ };
 
  const randomIntegerFromInterval = (min, max) => {
    return Math.floor(Math.random() * (max - min + 1) + min);
